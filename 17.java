7c18d01069bf6edd9308339a3487742199db81e2
saru502 <41730849+saru502@users.noreply.github.com>
2018-08-02 14:56:07 +0530

    Create 17.java

---
+++ 17.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num,number,temp,total=0;
+		Scanner Sc=new Scanner(System.in);
+		num=Sc.nextInt();
+		number=num;
+		while(number!=0)
+		{
+			temp=number%10;
+			total=total+temp*temp*temp;
+			number=number/10;
+		}
+		if(total==num)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}
