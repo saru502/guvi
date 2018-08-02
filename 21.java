f0c590622668b61a7be1dd4c012fcee66a216647
saru502 <41730849+saru502@users.noreply.github.com>
2018-08-02 16:20:54 +0530

    Create 21.java

---
+++ 21.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,n,d,k;
+		Scanner Sc=new Scanner(System.in);
+		a=Sc.nextInt();
+		n=Sc.nextInt();
+		d=Sc.nextInt();
+		k=(n*((2*a)+(n-1)*d))/2;
+		System.out.println(k);
+		
+	}
+}
