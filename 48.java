a8812fb938d630df2edf9799cbe6b9d015303d64
saru502 <41730849+saru502@users.noreply.github.com>
2018-08-16 12:33:26 +0530

    Create 48.java

---
+++ 48.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,t,i,sum=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			sum=sum+a[i];
+		}
+		t=sum/n;
+			System.out.println(t);
+	}
+}
+		
+				
+			
+		
+	
