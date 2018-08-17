264ac6c6151cd2b41d9051a8ecb5f95537f0f565
saru502 <41730849+saru502@users.noreply.github.com>
2018-08-17 07:52:35 +0530

    Update 52.java

--- 52.java
+++ 52.java
@@ -13,38 +13,40 @@ class Ideone
 		int n;
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
-		switch(ch);
+		switch(n)
+		{
 		case 1:
-			System.out.println("One");
+			System.out.println("one");
 			break;
 		case 2:
-			System.out.println("Two");
+			System.out.println("two");
 			break;
 		case 3:
-			System.out.println("Three");
+			System.out.println("three");
 			break;
 		case 4:
-			System.out.println("Four");
+			System.out.println("four");
 			break;
 		case 5:
-		     System.out.println("Five");
-		     break;
-		 case 6:
-	       	System.out.println("Six");
-	     	break;
-	     case 7:
-		   	System.out.println("Seven");
+		            System.out.println("five");
+		            break;
+		case 6:
+	       		System.out.println("six");
+	     		break;
+		case 7:
+		   	System.out.println("seven");
 		   	break;
-		 case 8:
-		 	System.out.println("Eight");
+		case 8:
+		 	System.out.println("eight");
 		 	break;
-		 case 9:
-		 	System.out.println("Nine");
+		case 9:
+		 	System.out.println("nine");
 		 	break;
-		 case 10:
-		 	System.out.println("Ten");
+		case 10:
+		 	System.out.println("ten");
 		 	break;
-		 default:;
-		 System.out.println("enter correct choice");
+		default:;
+			System.out.println("enter correct choice");
+		}
 	}
 }
