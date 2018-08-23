7b10a5c4e44c07c3f72e298d44b328dd6a5e754e
saru502 <41730849+saru502@users.noreply.github.com>
2018-08-23 14:49:03 +0530

    Create 65.java

---
+++ 65.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String s;
+		Scanner S=new Scanner(System.in);
+		s=S.next();
+		for(int i=0;i<1;System.out.println(s))
+		{
+			i++;
+		}
+	}
+}
