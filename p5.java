e951c1a0de62ce65bb2091508b8b3e5d464ce7c7
saru502 <41730849+saru502@users.noreply.github.com>
2018-09-04 11:03:44 +0530

    Create p5.java

---
+++ p5.java
@@ -0,0 +1,47 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String s;
+		Scanner Sc=new Scanner(System.in);
+		Sc=s.next().toUpperCase();
+		switch(s)
+		{
+			case "I":
+				System.out.println("1");
+				break;
+			case "II":
+				System.out.println("2");
+				break;
+			case "III":
+					System.out.println("3");
+					break;
+			case "IV":
+					System.out.println("4");
+					break;
+			case "V":
+						System.out.println("5");
+						break;
+			case "VI":
+						System.out.println("6");
+						break;
+			case "VII":
+				System.out.println("7");
+				break;
+				case "VIII";
+				System.out.println("8");
+				break;
+				case "IX":
+					System.out.println("9");
+					break;
+							}
+	}
+}
